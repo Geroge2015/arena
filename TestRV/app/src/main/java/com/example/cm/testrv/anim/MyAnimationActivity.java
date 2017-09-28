1172f4b640ce1f7a631c6e696f5351c49878db5d
George2015 <sunnyfrank2008@126.com>
2017-09-28 11:29:20 +0800

    [update] correct layout; thumbtint.

--- TestRV/app/src/main/java/com/example/cm/testrv/anim/MyAnimationActivity.java
+++ TestRV/app/src/main/java/com/example/cm/testrv/anim/MyAnimationActivity.java
@@ -15,7 +15,7 @@ public class MyAnimationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.my_animation_layout);
 //        init1();
 //        initPoint();
     }
