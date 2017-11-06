96f0cb9893ebc30cbe2b1bc80a882da4c9ffd951
George2015 <sunnyfrank2008@126.com>
2017-11-06 14:36:02 +0800

    [update] test

---
+++ TestRV/app/src/main/java/com/example/cm/testrv/persistence/MyNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.cm.testrv.persistence;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cm on 2017/10/31.
+ */
+
+public class MyNoteActivity extends AppCompatActivity {
+
+
+    // FIXME: 2017/11/6
+
+}
---
+++ TestRV/app/src/main/java/com/example/cm/testrv/utils/MySystemUtils.java
@@ -0,0 +1,56 @@
+package com.example.cm.testrv.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Locale;
+
+/**
+ * Created by George on 2017/10/20.
+ *
+ */
+
+public class MySystemUtils {
+
+    public static String getLanguage() {
+        return Locale.getDefault().getLanguage();
+    }
+
+    public static String getFullNameLan() {
+        return Locale.getDefault().getDisplayName();
+    }
+
+    public static int getFullScreenHeight(Context context) {
+        WindowManager wm = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE));
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        return dm.heightPixels;
+    }
+
+
+    public static int getStartEndHour(String span, boolean isStart, int defTime) {
+        if (!TextUtils.isEmpty(span)) {
+            try {
+                String[] items = span.split(",");
+                Log.d("GeorgeGo", "items " + items);
+                if (2 == items.length) {
+                    int start = Integer.valueOf(items[0]);
+                    int end = Integer.valueOf(items[1]);
+                    if (start >= 0 && end <= 24 && start <= end) {
+                        return isStart ? start : end;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return defTime;
+
+
+    }
+
+}
