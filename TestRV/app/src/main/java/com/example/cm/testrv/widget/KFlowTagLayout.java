cebdbe70a27c349c1a2465be6938e9a9e33ef547
George2015 <sunnyfrank2008@126.com>
2018-02-07 20:15:44 +0800

    [update] write/read bean data

--- TestRV/app/src/main/java/com/example/cm/testrv/widget/KFlowTagLayout.java
+++ TestRV/app/src/main/java/com/example/cm/testrv/widget/KFlowTagLayout.java
@@ -2,6 +2,7 @@ package com.example.cm.testrv.widget;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -20,12 +21,72 @@ public class KFlowTagLayout extends ViewGroup {
 
     @Override
     public LayoutParams generateLayoutParams(AttributeSet attrs) {
-        return super.generateLayoutParams(attrs);
+        return new MarginLayoutParams(getContext(), attrs);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        //获取Padding     获得它的父容器为它设置的测量模式和大小
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        //FlowLayout最终的宽度和高度值
+        int resultWidth = 0;
+        int resultHeight = 0;
+        //测量时每一行的宽度
+        int lineWidth = 0;
+        //测量时每一行的高度，加起来就是FlowLayout的高度
+        int lineHeight = 0;
+
+        //遍历每个子元素
+        for (int i = 0, childCount = getChildCount(); i < childCount; i++) {
+            View childView = getChildAt(i);
+            //测量每一个子view的宽和高
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec);
+
+            //获取到测量的宽和高
+            int childWidth = childView.getMeasuredWidth();
+            int childHeight = childView.getMeasuredHeight();
+
+            //因为子View可能设置margin，这里要加上margin的距离
+            MarginLayoutParams mlp = (MarginLayoutParams) childView.getLayoutParams();
+            int realChildWidth = childWidth + mlp.leftMargin + mlp.rightMargin;
+            int realChildHeight = childHeight + mlp.topMargin + mlp.bottomMargin;
+
+            //如果当前一行的宽度加上要加入的子view的宽度大于父容器给的宽度，就换行
+            if ((lineWidth + realChildWidth) > widthSize) {
+                //换行
+                resultWidth = Math.max(lineWidth, realChildWidth);
+                resultHeight += realChildHeight;
+                //换行了，lineWidth和lineHeight重新算
+                lineWidth = realChildWidth;
+                lineHeight = realChildHeight;
+            } else {
+                //不换行，直接相加
+                lineWidth += realChildWidth;
+                //每一行的高度取二者最大值
+                lineHeight = Math.max(lineHeight, realChildHeight);
+            }
+
+            //遍历到最后一个的时候，肯定走的是不换行。
+            if (i == childCount - 1) {
+                resultWidth = Math.max(lineWidth, resultWidth);
+                resultHeight += lineHeight;
+            }
+
+            setMeasuredDimension(widthMode == MeasureSpec.EXACTLY ? widthSize : resultWidth,
+                    heightMode == MeasureSpec.EXACTLY ? heightSize : resultHeight);
+
+        }
+
+
+
+
+
     }
 
     @Override
